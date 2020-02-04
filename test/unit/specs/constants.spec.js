360e88445bea6c1b5f31654b5553c26da28a0cf7
Chris Williams <chris.a.williams@gmail.com>
2020-02-04 14:40:15 -0500

    test: fix module references

--- test/unit/specs/constants.spec.js
+++ test/unit/specs/constants.spec.js
@@ -18,15 +18,15 @@ describe('ti.geofence', () => {
 		if (IOS) {
 			describe('REGION_STATE_*', () => {
 				it('REGION_STATE_UNKNOWN', () => {
-					expect(Map.REGION_STATE_UNKNOWN).toEqual(jasmine.any(Number));
+					expect(Geofence.REGION_STATE_UNKNOWN).toEqual(jasmine.any(Number));
 				});
 
 				it('REGION_STATE_INSIDE', () => {
-					expect(Map.REGION_STATE_INSIDE).toEqual(jasmine.any(Number));
+					expect(Geofence.REGION_STATE_INSIDE).toEqual(jasmine.any(Number));
 				});
 
 				it('REGION_STATE_OUTSIDE', () => {
-					expect(Map.REGION_STATE_OUTSIDE).toEqual(jasmine.any(Number));
+					expect(Geofence.REGION_STATE_OUTSIDE).toEqual(jasmine.any(Number));
 				});
 			});
 		}
@@ -34,39 +34,39 @@ describe('ti.geofence', () => {
 		if (ANDROID) {
 			describe('Google Play Services constants', () => {
 				it('SUCCESS', () => {
-					expect(Map.SUCCESS).toEqual(jasmine.any(Number));
+					expect(Geofence.SUCCESS).toEqual(jasmine.any(Number));
 				});
 
 				it('SERVICE_MISSING', () => {
-					expect(Map.SERVICE_MISSING).toEqual(jasmine.any(Number));
+					expect(Geofence.SERVICE_MISSING).toEqual(jasmine.any(Number));
 				});
 
 				it('SERVICE_VERSION_UPDATE_REQUIRED', () => {
-					expect(Map.SERVICE_VERSION_UPDATE_REQUIRED).toEqual(jasmine.any(Number));
+					expect(Geofence.SERVICE_VERSION_UPDATE_REQUIRED).toEqual(jasmine.any(Number));
 				});
 
 				it('SERVICE_DISABLED', () => {
-					expect(Map.SERVICE_DISABLED).toEqual(jasmine.any(Number));
+					expect(Geofence.SERVICE_DISABLED).toEqual(jasmine.any(Number));
 				});
 
 				it('SERVICE_INVALID', () => {
-					expect(Map.SERVICE_INVALID).toEqual(jasmine.any(Number));
+					expect(Geofence.SERVICE_INVALID).toEqual(jasmine.any(Number));
 				});
 
 				it('LOCATION_STATUS_ERROR', () => {
-					expect(Map.LOCATION_STATUS_ERROR).toEqual(jasmine.any(Number));
+					expect(Geofence.LOCATION_STATUS_ERROR).toEqual(jasmine.any(Number));
 				});
 
 				it('LOCATION_STATUS_GEOFENCE_NOT_AVAILABLE', () => {
-					expect(Map.LOCATION_STATUS_GEOFENCE_NOT_AVAILABLE).toEqual(jasmine.any(Number));
+					expect(Geofence.LOCATION_STATUS_GEOFENCE_NOT_AVAILABLE).toEqual(jasmine.any(Number));
 				});
 
 				it('LOCATION_STATUS_GEOFENCE_TOO_MANY_GEOFENCES', () => {
-					expect(Map.LOCATION_STATUS_GEOFENCE_TOO_MANY_GEOFENCES).toEqual(jasmine.any(Number));
+					expect(Geofence.LOCATION_STATUS_GEOFENCE_TOO_MANY_GEOFENCES).toEqual(jasmine.any(Number));
 				});
 
 				it('LOCATION_STATUS_GEOFENCE_TOO_MANY_PENDING_INTENTS', () => {
-					expect(Map.LOCATION_STATUS_GEOFENCE_TOO_MANY_PENDING_INTENTS).toEqual(jasmine.any(Number));
+					expect(Geofence.LOCATION_STATUS_GEOFENCE_TOO_MANY_PENDING_INTENTS).toEqual(jasmine.any(Number));
 				});
 			});
 		}
